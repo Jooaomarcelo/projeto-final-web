e31cc92473dcd3f20c5794086faeec0fb5f39645
Jooaomarcelo <joaomdsb@hotmail.com>
2024-11-13 19:13:21 -0300

    updated eslint, moved .gitignore, added .prettierrc

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default [
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    plugins: {
+      react: eslintPluginReact,
+      prettier: eslintPluginPrettier,
+      'react-hooks': eslintPluginReactHooks,
+      'jsx-a11y': eslintPluginJsxA11y,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'on',
+      'react/jsx-uses-react': 'on',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'jsx-a11y/anchor-is-valid': 'warn',
+    },
+  },
+];
