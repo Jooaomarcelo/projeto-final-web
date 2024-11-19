5b055b875dfad14094f253e4b8a8f3d0990df666
HenriUz <d2023001316@unifei.edu.br>
2024-11-19 09:20:32 -0300

    Added gradient to UserPage bg.

--- frontend/src/components/template/UserPage.jsx
+++ frontend/src/components/template/UserPage.jsx
@@ -10,7 +10,8 @@ export default function UserPage({ children }) {
 
   return (
     <section className="flex justify-around bg-[url(/unifei.jpg)] bg-cover bg-center h-screen">
-      <div className="h-screen flex items-center">
+      <div className="absolute inset-0 bg-gradient-to-l from-black/50 to-transparent"></div>
+      <div className="relative z-10 h-screen flex items-center">
         <button
           className="text-2xl text-white font-bold py-8 px-16 rounded-full bg-black/60"
           onClick={handleButtonClick}
@@ -18,7 +19,7 @@ export default function UserPage({ children }) {
           Explorar Repúblicas
         </button>
       </div>
-      <div className="h-screen flex items-center">{children}</div>
+      <div className="relative z-10 h-screen flex items-center">{children}</div>
     </section>
   );
 }
