a89936db140cc10a6cbaa9b394b40deaa714a173
gebra04 <gebrafacul@gmail.com>
2024-11-27 22:25:55 -0300

    add user auth to acess pages

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,35 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import { isSessionValid } from "./utils/auth";
 
-export function middleware(request){
-    if(request.nextUrl.pathname === "/"){
-        const url = request.nextUrl.clone();
-        url.pathname = "/login";
-        return NextResponse.redirect(url);
+//Esse "matcher" se encontra na própria documentação do next e serve para filtrar arquivos que não devem ser afetados
+export const config = {
+    matcher: '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.svg|.*\\.png|.*\\.jpg|.*\\.css|.*\\.js).*)'
+};
+
+const publicRoutes = [
+    '/home',
+    '/login',
+    '/signup'
+];
+
+
+// in ts export async function middleware(req: NextRequest) instead
+export async function middleware(req) {
+    const pathName = req.nextUrl.pathname;
+
+    // If the route is public, return the request
+    if (publicRoutes.includes(pathName)) {
+        return NextResponse.next();
     }
 
-    return NextResponse.next();
-}
+    // Else, check if the session is valid
+    const session = await isSessionValid();
+    if (session) {
+        return NextResponse.next();
+    }
 
-export const config = {
-    matcher: ["/"],
+    // If the session is invalid, redirect to login
+
+    return NextResponse.redirect(new URL('/login', req.url));
+
 }
---
+++ src/utils/auth.js
@@ -0,0 +1,48 @@
+import * as jose from 'jose';
+//npm i jose. The liberary is used to create and verify JWT tokens 
+// Also it's necessary to install npm i jsonwebtoken
+import { cookies } from 'next/headers';
+
+// in ts async function openSessionToken(token: string) instead
+async function openSessionToken(token) {
+    const secret = new TextEnconder().enconde(process.env.TOKEN);
+    const { payload } = await jose.jwtVerify(token, secret);
+
+    return payload;
+}
+
+export async function createSessionToken(payload = {}) {
+    const secret = new TextEnconder().enconde(process.env.TOKEN);
+
+    // Creating session token using SHA256 encryption
+    const session = await new jose.SignJWT(payload).setProtectedHeader({ alg: 'HS256' })
+        .setExpirationTime('2h') // set the expiration time to 2 hours 
+        .sign(secret); // sign the token
+
+    // Takin the expiration time from the token
+    const { exp } = await openSessionToken(session);
+
+    // Creating async cookie store
+    const cookieStore = await cookies();
+
+    cookieStore.set('session', session, {
+        expires: exp * 1000, // (exp as number) * 1000 in .ts
+        path: '/',
+        httpOnly: true,
+    });
+}
+
+export async function isSessionValid() {
+    const sessioncookie = (await cookies()).get('session');
+
+    if (!sessioncookie) {
+        return false;
+    }
+
+    const { value } = sessioncookie;
+    const { exp } = await openSessionToken(value);
+    const currentDate = new Date().getTime();
+
+    return (exp * 1000) > currentDate;
+}
+
