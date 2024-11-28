7c9a9623e8421b91ca5622e62f42f298139b1f1a
HenriUz <d2023001316@unifei.edu.br>
2024-11-28 20:13:27 -0300

    Added option to return just one fraternity on getFraternities.

--- src/utils/getFraternities.js
+++ src/utils/getFraternities.js
@@ -1,24 +1,25 @@
 import { readDB } from '@/utils/connectionDB';
 import { isSessionValid } from './auth';
+import { redirect } from 'next/navigation';
 
-export default async function readFraternities() {
+export default async function readFraternities(params = null) {
   const session = await isSessionValid();
   const fraternities = await readDB();
-  let fraternitiesTreated = [];
-
-  if (!session) {
-    for (let fraternity of fraternities) {
+  if (params) {
+    // Just one fraternity.
+    const fraternity = fraternities.find((frat) => frat.name === params.name);
+    /* Checking if the fraternity exists, if it doesn't exist it redirects. */
+    if (fraternity) {
       const { id, email, password, members, ...rest } = fraternity;
-
-      fraternitiesTreated.push(rest);
+      return session ? { ...rest, members } : rest;
+    } else {
+      redirect("/fraternities/Sbornia"); //Sbornia for now.
     }
   } else {
-    for (let fraternity of fraternities) {
-      const { id, email, password, ...rest } = fraternity;
-
-      fraternitiesTreated.push(rest);
-    }
+    //All fraternities.
+    return fraternities.map(frat => {
+      const { id, email, password, members, ...rest } = frat;
+      return session ? { ...rest, members } : rest;
+    });
   }
-
-  return fraternitiesTreated;
 }
