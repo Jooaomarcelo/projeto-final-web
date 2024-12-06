f9f84b97b0c790dfb8c579e226a1067956ed7446
HenriUz <d2023001316@unifei.edu.br>
2024-12-06 16:25:13 -0300

    Added css to fraternity page.

--- src/app/(pages)/fraternities/[fraternity]/page.jsx
+++ src/app/(pages)/fraternities/[fraternity]/page.jsx
@@ -10,19 +10,39 @@ export default async function Fraternity({ params }) {
     const editPermission = (await ownerToken()) === name; //Permission to edit the fraternity.
 
     return (
-        <section className="flex flex-col h-screen gap-3">
-            <div className="w-screen h-[40%] max-h-96 bg-[url(/unifei.jpg)]">
-                <img src={fraternity.image} alt="" className="w-auto h-[40%]"></img>
+        <section className="flex flex-col min-h-screen gap-3">
+            <div className="flex w-full h-auto max-h-[80vh] bg-[url(/unifei-campo.jpg)] bg-cover bg-no-repeat bg-[center_top_100%]">
+                <section className="flex flex-row items-end">
+                    <img src={fraternity.image} alt="" className="w-auto h-[60vh] pt-[10%]"></img>
+                    <h1 className="text-7xl text-white font-bold">{fraternity.name}</h1>
+                </section>
+            </div>
+            <div className="w-[80%] mx-auto mt-[1.5%] text-2xl">
+                <p>{fraternity.description}</p>
+                <section className="flex justify-between mt-[3vh]">
+                    <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
+                        <img src="/logo-sbornia.png" alt="sla" />
+                        <p className="my-auto">{fraternity.min_price} - {fraternity.max_price}</p>
+                    </div>
+                    <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
+                        <img src="/logo-sbornia.png" alt="sla" />
+                        <p className="my-auto">{fraternity.address.street}, {fraternity.address.res_number} - {fraternity.address.neighborhood}</p>
+                    </div>
+                    <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
+                        <img src="/logo-sbornia.png" alt="sla" />
+                        <p className="my-auto">{fraternity.whatsapp}</p>
+                    </div>
+                    <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
+                        <img src="/logo-sbornia.png" alt="sla" />
+                        <p className="my-auto">{fraternity.capacity}</p>
+                    </div>
+                </section>
+            </div>
+            <div className="flex justify-center gap-5 mt-[3vh]">
                 {editPermission && <FormUpdate fraternity={fraternity}></FormUpdate>}
+                {editPermission && <button>CONFIA</button>}
             </div>
             <div>
-                <h1 className="text-4xl text-black font-bold text-center">{fraternity.name}</h1>
-                <p>Descrição: {fraternity.description}</p>
-                <p>Capacidade: {fraternity.capacity}</p>
-                <p>Preço: {fraternity.min_price} - {fraternity.max_price}</p>
-                <p>Endereço: {fraternity.address.cep}</p>
-                <p>Contato: {fraternity.whatsapp}</p>
-                {session && <h1 className="text-4xl text-black font-bold text-center">Membros: </h1>}
                 {session && fraternity.members.map((member) => {
                     return <MemberCard member={member}></MemberCard>
                 })}
