f5c2fec25486f289e5244da29eba3f6523d992c4
Jooaomarcelo <joaomdsb@hotmail.com>
2024-12-07 17:37:53 -0300

    editing resizing in css

--- src/app/(pages)/fraternities/[fraternity]/page.jsx
+++ src/app/(pages)/fraternities/[fraternity]/page.jsx
@@ -15,16 +15,16 @@ export default async function Fraternity({ params }) {
   return (
     <section className="flex flex-col gap-3">
       <div className="flex flex-col min-h-screen w-full">
-        <div className="flex items-end w-full bg-[url(/unifei-campo.jpg)] bg-cover bg-no-repeat bg-[center_top_100%] pt-[10%]">
-          <section className="flex flw-wrap flex-row items-end">
-            <div className="w-[25%]">
+        <div className="flex items-end w-full min-h-[45vh] sm:min-h-[55vh] h-auto bg-[url(/unifei-campo.jpg)] bg-cover bg-no-repeat bg-[center_top_100%] pt-[10%]">
+          <section className="container flex flw-wrap flex-row items-end">
+            <div className="w-[25%] sm:w-[15%]">
               <img src={fraternity.image != '' ? fraternity.image : null} alt="" className="object-cover"></img>
             </div>
-            <h1 className="text-7xl text-white font-bold">{fraternity.name}</h1>
+            <h1 className="text-4xl sm:text-7xl text-white font-bold">{fraternity.name}</h1>
           </section>
         </div>
-        <div className="container mt-[1.5%] text-2xl">
-          <p>{fraternity.description}</p>
+        <div className="container mt-6 sm:mt-auto text-2xl">
+          <p className="text-base sm:text-lg">{fraternity.description}</p>
           <section className="flex flex-col items-center lg:flex-row flex-wrap gap-6 mt-6">
             <div className="h-16 min-w-[20%] px-[2%] py-[1%] flex justify-between items-center bg-gray-400 rounded-full">
               <Image src="/wallet.svg" height={48} width={48} alt="Preço" />
@@ -48,20 +48,20 @@ export default async function Fraternity({ params }) {
             </div>
           </section>
         </div>
-        <div className="flex flex-wrap w-[40%] mx-auto gap-5 mt-auto mb-6 text-white text-xl sm:text-2xl font-bold">
+        <div className="flex flex-wrap w-[40%] mx-auto gap-5 m-6 text-white text-xl sm:text-2xl font-bold">
           {editPermission && <FormUpdate fraternity={fraternity}></FormUpdate>}
           {editPermission && <FormDelete name={name}></FormDelete>}
         </div>
       </div>
       {/* Members section */}
-      <h2 className="text-center m-6 text-4xl font-bold">Membros</h2>
+      {session && <h2 className="text-center m-6 text-4xl font-bold">Membros</h2>}
       <div className="mb-6 container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
         {session &&
           fraternity.members.map((member) => {
             return <MemberCard key={member.id} member={member} editPermission={editPermission}></MemberCard>;
           })}
       </div>
-      {fraternity.members.length < fraternity.capacity && editPermission && (
+      {session && fraternity.members.length < fraternity.capacity && editPermission && (
         <FormFraternityMember action={'create'} fraternity={fraternity} />
       )}
     </section>
