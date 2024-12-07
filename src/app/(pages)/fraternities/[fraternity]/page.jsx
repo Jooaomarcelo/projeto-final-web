a7da0210a2ba58b3abd54598ffb151dfdc71818c
Jooaomarcelo <joaomdsb@hotmail.com>
2024-12-07 17:03:04 -0300

    added final styling in fraternity pages

--- src/app/(pages)/fraternities/[fraternity]/page.jsx
+++ src/app/(pages)/fraternities/[fraternity]/page.jsx
@@ -4,6 +4,7 @@ import MemberCard from '@/components/MemberCard';
 import FormUpdate from '@/components/FormUpdate';
 import FormFraternityMember from '@/components/FormFraternityMember';
 import FormDelete from '@/components/FormDelete';
+import Image from 'next/image';
 
 export default async function Fraternity({ params }) {
   const name = decodeURIComponent((await params).fraternity);
@@ -12,41 +13,45 @@ export default async function Fraternity({ params }) {
   const editPermission = (await ownerToken()) === name; //Permission to edit the fraternity.
 
   return (
-    <section className="flex flex-col min-h-screen gap-3">
-      <div className="flex w-full h-auto max-h-[80vh] bg-[url(/unifei-campo.jpg)] bg-cover bg-no-repeat bg-[center_top_100%]">
-        <section className="flex flex-row items-end">
-          <img src={fraternity.image != '' ? fraternity.image : null} alt="" className="w-auto h-[60vh] pt-[10%]"></img>
-          <h1 className="text-7xl text-white font-bold">{fraternity.name}</h1>
-        </section>
-      </div>
-      <div className="w-[80%] mx-auto mt-[1.5%] text-2xl">
-        <p>{fraternity.description}</p>
-        <section className="flex justify-between mt-[3vh]">
-          <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
-            <img src="/logo-sbornia.png" alt="sla" />
-            <p className="my-auto">
-              {fraternity.min_price} - {fraternity.max_price}
-            </p>
-          </div>
-          <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
-            <img src="/logo-sbornia.png" alt="sla" />
-            <p className="my-auto">
-              {fraternity.address.street}, {fraternity.address.res_number} - {fraternity.address.neighborhood}
-            </p>
-          </div>
-          <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
-            <img src="/logo-sbornia.png" alt="sla" />
-            <p className="my-auto">{fraternity.whatsapp}</p>
-          </div>
-          <div className="h-[5rem] px-[2%] py-[1%] flex gap-9 justify-evenly bg-gray-400 rounded-3xl">
-            <img src="/logo-sbornia.png" alt="sla" />
-            <p className="my-auto">{fraternity.capacity}</p>
-          </div>
-        </section>
-      </div>
-      <div className="flex justify-center gap-5 mt-[3vh]">
-        {editPermission && <FormUpdate fraternity={fraternity}></FormUpdate>}
-        {editPermission && <FormDelete name={name}></FormDelete>}
+    <section className="flex flex-col gap-3">
+      <div className="flex flex-col min-h-screen w-full">
+        <div className="flex items-end w-full bg-[url(/unifei-campo.jpg)] bg-cover bg-no-repeat bg-[center_top_100%] pt-[10%]">
+          <section className="flex flw-wrap flex-row items-end">
+            <div className="w-[25%]">
+              <img src={fraternity.image != '' ? fraternity.image : null} alt="" className="object-cover"></img>
+            </div>
+            <h1 className="text-7xl text-white font-bold">{fraternity.name}</h1>
+          </section>
+        </div>
+        <div className="container mt-[1.5%] text-2xl">
+          <p>{fraternity.description}</p>
+          <section className="flex flex-col items-center lg:flex-row flex-wrap gap-6 mt-6">
+            <div className="h-16 min-w-[20%] px-[2%] py-[1%] flex justify-between items-center bg-gray-400 rounded-full">
+              <Image src="/wallet.svg" height={48} width={48} alt="Preço" />
+              <span className="text-base ext-center">
+                {fraternity.min_price} - {fraternity.max_price}
+              </span>
+            </div>
+            <div className="h-16 px-[2%] py-[1%] flex min-w-[35%] justify-between items-center bg-gray-400 rounded-full">
+              <Image src="/local.svg" height={48} width={48} alt="Preço" />
+              <span className="text-base text-center">
+                {fraternity.address.street}, {fraternity.address.res_number} - {fraternity.address.neighborhood}
+              </span>
+            </div>
+            <div className="h-16 min-w-[20%] px-[2%] py-[1%] flex justify-between items-center bg-gray-400 rounded-full">
+              <Image src="/msg.svg" height={48} width={48} alt="Preço" />
+              <span className="text-base text-center">{fraternity.whatsapp}</span>
+            </div>
+            <div className="h-16 min-w-[15%] px-[2%] py-[1%] flex justify-between items-center bg-gray-400 rounded-full">
+              <Image src="/members.svg" height={48} width={48} alt="Preço" />
+              <span className="text-base">{fraternity.capacity}</span>
+            </div>
+          </section>
+        </div>
+        <div className="flex flex-wrap w-[40%] mx-auto gap-5 mt-auto mb-6 text-white text-xl sm:text-2xl font-bold">
+          {editPermission && <FormUpdate fraternity={fraternity}></FormUpdate>}
+          {editPermission && <FormDelete name={name}></FormDelete>}
+        </div>
       </div>
       {/* Members section */}
       <h2 className="text-center m-6 text-4xl font-bold">Membros</h2>
--- src/components/FormDelete.jsx
+++ src/components/FormDelete.jsx
@@ -1,61 +1,66 @@
-"use client";
+'use client';
 
-import { deleteSessionToken } from "@/utils/auth";
-import { deleteFraternity } from "@/utils/crudFraternities";
-import { useState } from "react"
-import toast, { Toaster } from "react-hot-toast";
+import { deleteSessionToken } from '@/utils/auth';
+import { deleteFraternity } from '@/utils/crudFraternities';
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function FormDelete({ name }) {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    /*
+  /*
     Description: functions responsibles for changing de visibility of the form.
     */
-    const handleOpenForm = () => setIsOpen(true);
-    const handleCloseForm = () => setIsOpen(false);
+  const handleOpenForm = () => setIsOpen(true);
+  const handleCloseForm = () => setIsOpen(false);
 
-    /*
+  /*
     Description: function responsible for calling the verification function and preventing the form from submitting directly.
     */
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        /* Checking input. */
-        if (await deleteFraternity(name, e.target.elements.password.value)) {
-            toast.success("República deletada com sucesso.", { duration: 5000 });
-            await deleteSessionToken();
-        } else {
-            toast.error("Senha incorreta.");
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    /* Checking input. */
+    if (await deleteFraternity(name, e.target.elements.password.value)) {
+      toast.success('República deletada com sucesso.', { duration: 5000 });
+      await deleteSessionToken();
+    } else {
+      toast.error('Senha incorreta.');
     }
+  };
 
-    return (
-        <>
-            <button onClick={handleOpenForm} className="text-white text-3xl font-bold bg-red-700 rounded-md p-3">Excluir</button>
-            {isOpen &&
-                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
-                    <div className="user-form max-w-lg w-full">
-                        <div className="relative">
-                            <button type="button" onClick={handleCloseForm} className="absolute top-2 right-2 text-white hover:text-black font-bold">X</button>
-                        </div>
-                        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-                            <h1 className="text-2xl text-center font-bold">Deseja mesmo excluir a república?</h1>
-                            <div>
-                                <section className="flex gap-2 items-center">
-                                    <label htmlFor="password" className="text-nowrap text-lg font-bold">Confirme a senha: </label>
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        className="flex-grow input-user-update"
-                                    />
-                                </section>
-                            </div>
-                            <button className="text-blue-950 text-3xl font-bold bg-white h-14 py-2 rounded-full">Excluir</button>
-                        </form>
-                        <Toaster></Toaster>
-                    </div>
-                </div>
-            }
-        </>
-    )
+  return (
+    <>
+      <button onClick={handleOpenForm} className="flex-1 bg-red-700 rounded-md p-3">
+        Excluir
+      </button>
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+          <div className="user-form max-w-lg w-full">
+            <div className="relative">
+              <button
+                type="button"
+                onClick={handleCloseForm}
+                className="absolute top-2 right-2 text-white hover:text-black font-bold"
+              >
+                X
+              </button>
+            </div>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+              <h1 className="text-2xl text-center font-bold">Deseja mesmo excluir a república?</h1>
+              <div>
+                <section className="flex gap-2 items-center">
+                  <label htmlFor="password" className="text-nowrap text-lg font-bold">
+                    Confirme a senha:{' '}
+                  </label>
+                  <input type="password" id="password" name="password" className="flex-grow input-user-update" />
+                </section>
+              </div>
+              <button className="text-blue-950 text-3xl font-bold bg-white h-14 py-2 rounded-full">Excluir</button>
+            </form>
+            <Toaster></Toaster>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
