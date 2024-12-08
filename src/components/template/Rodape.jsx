1e66e76a49bd0f0b42155c4b4cb0e4a2d218f16d
HenriUz <d2023001316@unifei.edu.br>
2024-12-08 10:24:19 -0300

    Added github on footer.

--- src/components/template/Rodape.jsx
+++ src/components/template/Rodape.jsx
@@ -6,22 +6,22 @@ export default function Rodape() {
       <h3 className="text-xl font-bold">Desenvolvido por:</h3>
       <ul className="flex gap-6 justify-center">
         <li>
-          <Link href="#">
+          <Link href="https://github.com/Jooaomarcelo" target="_blank">
             <div className="a-user-unlogged"></div>
           </Link>
         </li>
         <li>
-          <Link href="#">
+          <Link href="https://github.com/HenriUz" target="_blank">
             <div className="a-user-unlogged"></div>
           </Link>
         </li>
         <li>
-          <Link href="#">
+          <Link href="https://github.com/gebra04" target="_blank">
             <div className="a-user-unlogged"></div>
           </Link>
         </li>
         <li>
-          <Link href="#">
+          <Link href="https://github.com/ValimD" target="_blank">
             <div className="a-user-unlogged"></div>
           </Link>
         </li>
