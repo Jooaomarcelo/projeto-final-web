098aee150cc45e8869681e040d89041d8f7d76cd
HenriUz <d2023001316@unifei.edu.br>
2024-12-09 11:19:22 -0300

    mend

--- src/components/UserButton.jsx
+++ src/components/UserButton.jsx
@@ -1,33 +1,44 @@
 'use client';
 
 import { ownerToken } from '@/utils/auth';
+import { getFraternity } from '@/utils/crudFraternities';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 export default function UserButton({ action, ref }) {
   const [name, setName] = useState();
+  const [data, setData] = useState({ name: "", image: "" });
 
   const getOwnerSession = async () => {
     const name = await ownerToken();
-    setName(name);
+    //setName(name);
+
+    const fraternity = await getFraternity({ name });
+    if (fraternity) {
+      setData({ name: fraternity.name, image: fraternity.image });
+    } else {
+      setData({ name: "", image: "" });
+    }
   };
+
   useEffect(() => {
     getOwnerSession();
+    console.log(data);
   }, []);
 
   return (
     <button className="h-10 w-10 rounded-full bg-cover bg-center" onClick={action} ref={ref}>
-      {typeof name === 'string' ? (
+      {(data.image !== "") ? (
         <Image
-          src={`/logo-${name.toLowerCase().split(' ')[0]}.png`}
+          src={data.image != "" ? data.image : "/icons/user-unlogged.svg"}
           height={100}
           width={100}
-          alt="Usuário deslogado "
+          alt="Usuário logado "
           className="rounded-full"
         />
       ) : (
         <Image
-          src="/user-unlogged.svg"
+          src="/icons/user-unlogged.svg"
           height={100}
           width={100}
           alt="Usuário deslogado "
