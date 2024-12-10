622365fb8c4748e13d2e20d3a6b54c6b820f8e6e
Jooaomarcelo <joaomdsb@hotmail.com>
2024-12-10 11:28:44 -0300

    Added developers photos

--- src/components/template/Rodape.jsx
+++ src/components/template/Rodape.jsx
@@ -6,23 +6,23 @@ export default function Rodape() {
       <h3 className="text-xl font-bold">Desenvolvido por:</h3>
       <ul className="flex gap-6 justify-center">
         <li>
-          <Link href="https://github.com/Jooaomarcelo" target="_blank">
-            <div className="a-user-unlogged"></div>
+          <Link href="https://github.com/Jooaomarcelo" target="_blank" title="João Marcelo">
+            <div className="h-20 w-20 rounded-full bg-cover bg-center bg-[url(/devs/zang.jpg)]"></div>
           </Link>
         </li>
         <li>
-          <Link href="https://github.com/HenriUz" target="_blank">
-            <div className="a-user-unlogged"></div>
+          <Link href="https://github.com/HenriUz" target="_blank" title="Henrique Zucato">
+            <div className="h-20 w-20 rounded-full bg-cover bg-center bg-[url(/devs/videco.jpg)]"></div>
           </Link>
         </li>
         <li>
-          <Link href="https://github.com/gebra04" target="_blank">
-            <div className="a-user-unlogged"></div>
+          <Link href="https://github.com/gebra04" target="_blank" title="Murilo Coelho">
+            <div className="h-20 w-20 rounded-full bg-cover bg-[center_top] bg-[url(/devs/zizu.jpg)]"></div>
           </Link>
         </li>
         <li>
-          <Link href="https://github.com/ValimD" target="_blank">
-            <div className="a-user-unlogged"></div>
+          <Link href="https://github.com/ValimD" target="_blank" title="Diego Valim">
+            <div className="h-20 w-20 rounded-full bg-cover bg-[left_bottom] bg-[url(/devs/mm.jpg)]"></div>
           </Link>
         </li>
       </ul>
